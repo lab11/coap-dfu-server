04f993e0f4151b8c5ab960bb53e0654276ab9572
Neal Jackson <nealjack@umich.edu>
2021-01-26 21:05:05 -0800

    got running locally

--- dfu-webhook-endpoint.py
+++ dfu-webhook-endpoint.py
@@ -6,6 +6,13 @@ import os
 app = Flask(__name__)  # Standard Flask app
 webhook = Webhook(app) # Defines '/postreceive' endpoint
 
+app_path = os.environ.get('APP_PATH')
+pkg_path = os.environ.get('PKG_PATH')
+
+if not os.path.exists(pkg_path):
+    print('building!')
+    sh.make('-C', app_path, 'pkg_signed')
+
 @app.route("/")        # Standard Flask endpoint
 def hello_world():
     return "Hello, World!"
@@ -13,7 +20,8 @@ def hello_world():
 @webhook.hook()        # Defines a handler for the 'push' event
 def on_push(data):
     print("Got push with: {0}".format(data))
-    sh.make('-C ' + os.environ.get('APP_PATH'))
+    sh.make('-C', app_path, 'clean')
+    sh.make('-C', app_path, 'pkg_signed')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
---
+++ dfu_entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 coap-dfu-server.py -pkg $PKG_PATH
